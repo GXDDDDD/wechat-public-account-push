bf980e195f8a1374492dc9719404eaa80288d149
GXDDDDD <115146549+GXDDDDD@users.noreply.github.com>
2022-10-07 09:14:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f2d2f6b47f3d0a83de62d5a1424557b1',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '辽宁',
+  CITY: '沈阳',
 
   USERS: [
     {
